7ff6ffab3b724b77f42563f26798b0820145b736
Jarvis_N <48059804+jarvisN@users.noreply.github.com>
2021-10-01 15:17:23 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
-host = '192.168.1.34'       # IP host 
+host = ''       # IP host 
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
